fb7f822fedbf1b63269f36a8250ecb385693890e
ClauHenrique <johnhenriqueoliveira2021@gmail.com>
2024-02-08 19:57:11 -0300

    definir qual arquivo deve ser renomeado

--- rename-files.sh
+++ rename-files.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
+
+rename() {
+    path=$1
+    file=$2
+
+    date=$(stat -c %y "$file") 
+    hour=${date:11:8}
+    extension="${file##*.}"
+
+
+    if [ "$3" ]; then
+
+        file_extension=$3
+
+        # so converter arquivos com a extensao especificada no segundo parametro
+        if [ "$extension" = "$file_extension" ]; then
+            new_name="$hour.$extension"
+
+            mv "$file" "$path/$new_name"
+
+        fi
+
+    else
+        new_name="$hour.$extension"
+
+        mv "$file" "$path/$new_name"
+    fi
+}
+
+
 path="$1"
+extensao="$2"
 
 for file in "$path"/*; do
 
     if [ -f "$file" ]; then
 
-        date=$(stat -c %y "$file") 
-        hour=${date:11:8}
-        extension="${file##*.}"
-
-        new_name="$hour.$extension"
-
-        mv "$file" "$path/$new_name"
+        rename "$path" "$file" "$extensao"
 
     else
         echo ">> $file"
